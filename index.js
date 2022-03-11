fb4190076fca9cf5ca8f64391c14dc99448bd292
Ludo <ludovic@codekraft.fr>
2022-03-11 11:12:18 +0100

    Fix dependency issue, react-native doesn't hold AsyncStorage anymore

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 let getState = async () => {
     let savedState = await AsyncStorage.getItem('state')
